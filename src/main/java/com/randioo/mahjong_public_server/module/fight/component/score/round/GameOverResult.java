978c394a2cd764ad476cc6da7dac80a3067f5deb
aimfd <1101697681@qq.com>
2017-08-06 13:43:56 +0800

    抓胡二次结算次数bug修复,优化对象打印

--- src/main/java/com/randioo/mahjong_public_server/module/fight/component/score/round/GameOverResult.java
+++ src/main/java/com/randioo/mahjong_public_server/module/fight/component/score/round/GameOverResult.java
@@ -7,35 +7,39 @@ package com.randioo.mahjong_public_server.module.fight.component.score.round;
  *
  */
 public class GameOverResult extends RoundOverResult {
-    /** 胡的次数 */
-    public int huCount;
-    /** 摸胡次数 */
-    public int moHuCount;
-    /** 抓胡次数 */
-    public int zhuaHuCount;
-    /** 点冲次数 */
-    public int dianChong;
-    /** 分数 */
-    public int score;
-    /** 明杠分数 */
-    public int mingGangScore;
-    /** 明杠次数 */
-    public int mingGangCount;
-    /** 暗杠分数 */
-    public int darkGangScore;
-    /** 暗杠次数 */
-    public int dardGangCount;
-    /** 补杠分数 */
-    public int addGangScore;
-    /** 补杠次数 */
-    public int addGangCount;
+	/** 胡的次数 */
+	public int huCount;
+	/** 摸胡次数 */
+	public int moHuCount;
+	/** 抓胡次数 */
+	public int zhuaHuCount;
+	/** 点冲次数 */
+	public int dianChong;
+	/** 分数 */
+	public int score;
+	/** 明杠分数 */
+	public int mingGangScore;
+	/** 明杠次数 */
+	public int mingGangCount;
+	/** 暗杠分数 */
+	public int darkGangScore;
+	/** 暗杠次数 */
+	public int dardGangCount;
+	/** 补杠分数 */
+	public int addGangScore;
+	/** 补杠次数 */
+	public int addGangCount;
 
-    @Override
-    public String toString() {
-        return "GameOverResult [huCount=" + huCount + ", moHuCount=" + moHuCount + ", zhuaHuCount=" + zhuaHuCount
-                + ", dianChong=" + dianChong + ", score=" + score + ", mingGangScore=" + mingGangScore
-                + ", mingGangCount=" + mingGangCount + ", darkGangScore=" + darkGangScore + ", dardGangCount="
-                + dardGangCount + ", addGangScore=" + addGangScore + ", addGangCount=" + addGangCount + "]";
-    }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("GameOverResult [huCount=").append(huCount).append(", moHuCount=").append(moHuCount)
+				.append(", zhuaHuCount=").append(zhuaHuCount).append(", dianChong=").append(dianChong)
+				.append(", score=").append(score).append(", mingGangScore=").append(mingGangScore)
+				.append(", mingGangCount=").append(mingGangCount).append(", darkGangScore=").append(darkGangScore)
+				.append(", dardGangCount=").append(dardGangCount).append(", addGangScore=").append(addGangScore)
+				.append(", addGangCount=").append(addGangCount).append("]");
+		return builder.toString();
+	}
 
 }
--- src/main/java/com/randioo/mahjong_public_server/module/fight/component/score/round/RoundOverResult.java
+++ src/main/java/com/randioo/mahjong_public_server/module/fight/component/score/round/RoundOverResult.java
@@ -3,56 +3,39 @@ package com.randioo.mahjong_public_server.module.fight.component.score.round;
 import com.randioo.mahjong_public_server.protocol.Entity.OverMethod;
 
 public class RoundOverResult {
-    /** 座位号 */
-    public int seat;
-    /** 分数 */
-    public int score;
-    /** 结束原因 */
-    public OverMethod overMethod;
-    /** 是否杠开 */
-    public boolean gangKai;
-    /** 明杠分数 */
-    public int mingGangScore;
-    /** 明杠次数 */
-    public int mingGangCount;
-    /** 暗杠分数 */
-    public int darkGangScore;
-    /** 暗杠次数 */
-    public int dardGangCount;
-    /** 补杠分数 */
-    public int addGangScore;
-    /** 补杠次数 */
-    public int addGangCount;
-    /** 自摸 */
-    public int moScore;
+	/** 座位号 */
+	public int seat;
+	/** 分数 */
+	public int score;
+	/** 结束原因 */
+	public OverMethod overMethod;
+	/** 是否杠开 */
+	public boolean gangKai;
+	/** 明杠分数 */
+	public int mingGangScore;
+	/** 明杠次数 */
+	public int mingGangCount;
+	/** 暗杠分数 */
+	public int darkGangScore;
+	/** 暗杠次数 */
+	public int dardGangCount;
+	/** 补杠分数 */
+	public int addGangScore;
+	/** 补杠次数 */
+	public int addGangCount;
+	/** 自摸 */
+	public int moScore;
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("RoundOverResult [seat=");
-        builder.append(seat);
-        builder.append(", score=");
-        builder.append(score);
-        builder.append(", overMethod=");
-        builder.append(overMethod);
-        builder.append(", gangKai=");
-        builder.append(gangKai);
-        builder.append(", mingGangScore=");
-        builder.append(mingGangScore);
-        builder.append(", mingGangCount=");
-        builder.append(mingGangCount);
-        builder.append(", darkGangScore=");
-        builder.append(darkGangScore);
-        builder.append(", dardGangCount=");
-        builder.append(dardGangCount);
-        builder.append(", addGangScore=");
-        builder.append(addGangScore);
-        builder.append(", addGangCount=");
-        builder.append(addGangCount);
-        builder.append(", moScore=");
-        builder.append(moScore);
-        builder.append("]");
-        return builder.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("RoundOverResult [seat=").append(seat).append(", score=").append(score).append(", overMethod=")
+				.append(overMethod).append(", gangKai=").append(gangKai).append(", mingGangScore=")
+				.append(mingGangScore).append(", mingGangCount=").append(mingGangCount).append(", darkGangScore=")
+				.append(darkGangScore).append(", dardGangCount=").append(dardGangCount).append(", addGangScore=")
+				.append(addGangScore).append(", addGangCount=").append(addGangCount).append(", moScore=")
+				.append(moScore).append("]");
+		return builder.toString();
+	}
 
 }
